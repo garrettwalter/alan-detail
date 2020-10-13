3b115d31cb34b881b19b662193d341ba60b9813e
Garrett Walter <gtwalter150@gmail.com>
2020-10-13 16:17:00 -0400

    created navbar and background

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#alan").css("background-image", "url('https://www.theworksautocenter.com/wp-content/uploads/2020/05/car-wash-near-me-3.jpg')");
+
+
+
+    
+ });
