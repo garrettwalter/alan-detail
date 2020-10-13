4dabfb9c56d214159a38dd0c35aaa46e18b90da1
Garrett Walter <gtwalter150@gmail.com>
2020-10-13 18:57:39 -0400

    nav links display sckeletons of content

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-    $("#alan").css("background-image", "url('https://www.theworksautocenter.com/wp-content/uploads/2020/05/car-wash-near-me-3.jpg')");
 
+    var url = ["https://www.theworksautocenter.com/wp-content/uploads/2020/05/car-wash-near-me-3.jpg"]
 
-
+    $("#alan").css("background-image", "url(" + url + ")");
+    
+    $("#bookNow").hide();
+    $("#socialMedia").hide();
+    $("#meetAlan").hide();
+    
+    $("#book-now").on("click", function(){
+        $("#bookNow").css("display","block");
+        $("#socialMedia").hide();
+        $("#meetAlan").hide();
+    })
+    $("#social-media").on("click", function(){
+        $("#socialMedia").css("display","block");
+        $("#bookNow").hide();
+        $("#meetAlan").hide();
+    })
+    $("#meet-alan").on("click", function(){
+        $("#meetAlan").css("display","block");
+        $("#bookNow").hide();
+        $("#socialMedia").hide();
+    })
     
  });
